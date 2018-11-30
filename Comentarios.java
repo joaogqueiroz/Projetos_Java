1810da958d490d9d0e6b75debf849e5c6bb64a48
João Gilberto de Queiroz <joaogilberto350@hotmail.com>
2018-11-30 02:32:26 -0200

    arquivos criados ate o momento

---
+++ Comentarios.java
@@ -0,0 +1,27 @@
+package fundamentos;
+/** Esse método é a portda de entrada de um programa Java
+ * 
+ * @param args
+ *            parâmetros passados por linha de comando
+ * @author João
+ * @version 0.1
+ * @since 1.0
+ * 
+ */
+
+public class Comentários {
+	public static void main(String[] args) {
+		/** Imprime no console a palavra 'Primeiro' e em seguida pula para proxima linha
+		 * 
+		 * 
+		 */
+		
+		System.out.println("primeiro ");
+		//Cuidado como os comentários óbvios
+		System.out.println("programa!");
+		
+		
+		
+	}
+
+}
---
+++ Primeiro_programa.java
@@ -0,0 +1,8 @@
+package fundamentos;
+
+public class Primeiro_programa {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD!");
+	}
+}
